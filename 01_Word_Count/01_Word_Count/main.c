57c1c7325558a3d94a058e5e6ccb66fb4160f97a
John Choi <chiefmasamune@gmail.com>
2024-03-18 23:08:16 -0700

    Change count_lines() to check character-by-character, instead of up to 1kb chunks. Add count_words() function. Reorganize main().

--- 01_Word_Count/01_Word_Count/main.c
+++ 01_Word_Count/01_Word_Count/main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <ctype.h>
 
 FILE *f = NULL;
 
@@ -15,19 +16,45 @@ int count_bytes()
 
 int count_lines()
 {
-	char buf = '\0'; //Assuming a single line of the file won't be longer than 1 kilobyte
+	char buf = '\0';
 	int count = 0;
 
 	while ((buf = fgetc(f)) != EOF) //Could be faster to use getline or fgets, but then have to mess around a lot more with buffers
-		if (buf == '\n' || buf == '\r') //Check newline for Windows and carriage return for Linux
+		if (buf == '\n') 
 			count++;
 
 	return count;
 }
 
-/*
-int count_
-*/
+//Could be refined to handle poor grammar and such, but I'm going to (foolishly) believe the user will provide good data.
+int count_words()
+{
+	char buf = '\0';
+	int count = 0, flag = 0;
+
+	while ((buf = fgetc(f)) != EOF)
+	{
+		if (buf < 0)
+		{
+			flag = 1; //POSIX definition of a word is some amount of non-whitespace characters delimited by whitespace characters, so yeah. 
+			continue;
+		}
+		else if (isspace(buf))
+		{
+			if (flag == 1) //Make sure consecutive whitespace isn't counted as multiple words
+			{
+				count++;
+				flag = 0;
+			}
+		}
+		else
+			flag = 1; //Mark that previous character wasn't whitespace, which I'm going to assume means it's a normal character.
+	}
+
+	return count;
+}
+
+
 
 int open_file(const char *path)
 {
@@ -43,26 +70,56 @@ int open_file(const char *path)
 
 int main (int argc, char **argv)
 {
-	//Make sure there is a flag for now. 
-	if (argc < 3) 
+	if (argc == 1)
+		printf("Please pass a flag (-c, -l, -w) and/or a file.\n");
+	else if (argc == 2)
 	{
-		printf ("Call this program with a flag (-c, -l, -w, -m).\n");
-		return 1;
-	}
-	
-	if (open_file(argv[2]) != 0)
-		return 1;
+		if (argv[1][0] == '-')
+		{
+			//flag passed in, but no filepath, so get data from stdin
+		}
+		else //Byte, line, and word count for the file
+		{
+			if (open_file(argv[1]) != 0)
+				return 1;
 
-	switch (argv[1][1]) {
-	case 'c':
-		printf("%d characters in file.\n", count_bytes(argv[2]));
-		break;
-	case 'l':
-		printf ("%d lines in file.\n", count_lines(argv[2]));
-		break;
-	default:
-		printf ("Please enter a valid flag: -c, -l, -w , -m\n");
+			int c = 0, l = 0, w = 0;
+
+			c = count_bytes();
+			rewind(f);
+			l = count_lines();
+			rewind(f);
+			w = count_words();
+			printf("Bytes: %d, lines: %d, words: %d\n", c, l, w);
+		}
 	}
+	else if (argc == 3)
+	{
+		if (argv[1][0] == '-') //Run the specified operation on the file
+		{
+			if (open_file(argv[2]) != 0)
+				return 1;
+
+			switch (argv[1][1]) {
+			case 'c':
+				printf("%d characters in file.\n", count_bytes());
+				break;
+			case 'l':
+				printf ("%d lines in file.\n", count_lines());
+				break;
+			case 'w':
+				printf("%d words in file.\n", count_words());
+				break;
+			default:
+				printf ("Please enter a valid flag: -c, -l, -w\n");
+			}
+		}
+		else
+			printf("Second argument should be passed in as a flag; -c, -l, -w\n");
+
+	}
+	else
+		printf("Please pass one flag (-c, -l, -w) and/or a file, only.\n");
 
 	fclose(f);
 	return 0;
