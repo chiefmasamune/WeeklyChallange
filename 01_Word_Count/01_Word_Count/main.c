d18d9356aabf476514d09874d0c3b8ac79eab28d
John Choi <chiefmasamune@gmail.com>
2024-03-18 21:21:13 -0700

    Update the Readme with some more information on environment and testing procedures.
    Update Word_Count with a line count.

--- 01_Word_Count/01_Word_Count/main.c
+++ 01_Word_Count/01_Word_Count/main.c
@@ -1,36 +1,48 @@
 #include <stdio.h>
 
-int count_bytes(const char *path)
+FILE *f = NULL;
+
+int count_bytes()
 {
 	char buf[1024];
 	int count = 0, chunk = 0;
-	FILE *f = NULL;
-	int opened = fopen_s(&f, path, "r");
-	if (opened != 0) 
-	{
-		printf ("Error opening file: %s\n", path);
-		return -1;
-	}
 
 	while (chunk = fread(&buf, sizeof(char), 1024, f))
 		count += chunk;
 
-	fclose(f);
+	return count;
+}
+
+int count_lines()
+{
+	char buf = '\0'; //Assuming a single line of the file won't be longer than 1 kilobyte
+	int count = 0;
+
+	while ((buf = fgetc(f)) != EOF) //Could be faster to use getline or fgets, but then have to mess around a lot more with buffers
+		if (buf == '\n' || buf == '\r') //Check newline for Windows and carriage return for Linux
+			count++;
 
 	return count;
 }
 
-/*int count_lines(char *path)
-{
-
-}
-
+/*
 int count_
 */
 
+int open_file(const char *path)
+{
+	int opened = fopen_s(&f, path, "r");
+	if (opened != 0)
+	{
+		printf("Error opening file: %s\n", path);
+		return -1;
+	}
+
+	return 0;
+}
+
 int main (int argc, char **argv)
 {
-	int result = 0;
 	//Make sure there is a flag for now. 
 	if (argc < 3) 
 	{
@@ -38,17 +50,20 @@ int main (int argc, char **argv)
 		return 1;
 	}
 	
+	if (open_file(argv[2]) != 0)
+		return 1;
+
 	switch (argv[1][1]) {
 	case 'c':
-		result = count_bytes(argv[2]);
-		if (result > -1)
-			printf("%d characters in file.\n", result);
+		printf("%d characters in file.\n", count_bytes(argv[2]));
+		break;
+	case 'l':
+		printf ("%d lines in file.\n", count_lines(argv[2]));
 		break;
 	default:
 		printf ("Please enter a valid flag: -c, -l, -w , -m\n");
-		return 1;
 	}
 
-
+	fclose(f);
 	return 0;
 }
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Weekly Challenge
 My attempts at taking on the challenges on this site: https://codingchallenges.fyi/
+
+I'm working on the inferior OS, Windows, if the contents of the repo itself didn't make that obvious. 
+
+Also, I won't provide test files. "It works on my machine" is the motto for this repo.
